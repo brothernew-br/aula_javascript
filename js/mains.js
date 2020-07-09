185d172d05e9527a5ae10a0af90e247232800019
Thiago Pedro <omega_thirp@hotmail.com>
2020-07-09 17:16:36 -0300

    master

---
+++ js/mains.js
@@ -0,0 +1,27 @@
+
+
+
+/*var idade = prompt("Qual é sua idade");
+if(idade >= 18){
+    console.log("É maior de idade!")
+    alert("É maior de idade!")
+}else{
+    console.log("É menor de idade!")
+    alert("É menor de idade!")
+}
+*/
+
+/*var frutas=[{nome:"Maça", cor:"vermelha"},{nome:"Uva", cor:"Roxa"}];
+console.log(frutas);
+console.log(frutas[0].nome);
+console.log(frutas[1].cor);
+*/
+
+/*var fruta={nome:"Maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+//var nome = "Thiago Pedrozo";
+//alert("Bem vindo, "+nome+" ! -->alerta chamando arquivo js");
